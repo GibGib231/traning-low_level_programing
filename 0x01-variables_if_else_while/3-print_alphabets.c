7914eb32b73167cf67c00665d43357f00de15e0c
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-03-28 16:08:34 +0000

    this print both upper and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+	int low;
+	int upp;
+
+	for(low = 'a'; low <= 'z'; low++)
+
+{
+	putchar(low);
+}
+for (upp = 'A'; upp <= 'Z'; upp++)
+{
+	printf(upp);
+}
+       return (0);
+}
