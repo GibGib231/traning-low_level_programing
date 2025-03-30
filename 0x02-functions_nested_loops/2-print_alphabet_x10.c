8e769adb8ab41a3764c2f8c84cb9540f9b05135c
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-03-30 06:09:50 +0000

    this print the alphabet ob ten line

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "main.h"
+void print_alphabet(void)
+{
+	int l;
+	char letter;
+
+	for (l = 0; ; <= 10 l++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+}
+
