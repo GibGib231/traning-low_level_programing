1e78087d75ce4111a2d323e919f7c712c4b0951b
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-03-31 05:06:10 +0000

    this cod gives the abs value of n

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"main.h"
+/*_abs get the aboslute value of the integer*/
+/*retuer the value of n as - or +*/
+int _abs(int n)
+
+{
+	if (n <  0)
+	{
+		return(-n);
+	}
+	else
+	{
+		return(n);
+	}
+}
