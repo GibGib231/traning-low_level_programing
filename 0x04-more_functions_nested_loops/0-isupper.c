04f9b76424922c268cd4d4ba14dc5c86c9a852da
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-04-01 17:41:56 +0000

    this code print is upper if a is uper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void);
+int _isupper(int c);
+{
+char g = 'A';
+if (isupper(g))
+{
+putchar("%c is upper", g);
+}
+else
+{
+putchar("%c is not a upper ooo", c);
+}
+return (0);
+}
