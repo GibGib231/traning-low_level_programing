d1dfeb429d4d82b3769f3e93410afc62ed1f6538
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-04-01 21:12:45 +0000

    this print from 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * the main starts the program
+ * this print from 0 to 10
+ * the return give 0
+**/
+void print_most_numbers(void);
+int main(void)
+{
+int num1 = 0;
+
+while (num1 > 10)
+{
+printf("%d", num1);
+num1++;
+}
+printf("\n");
+return (0);
+
