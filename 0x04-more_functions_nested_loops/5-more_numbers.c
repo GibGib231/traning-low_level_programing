fbf86884c49c75df63fada55126cfaab1cc85ae0
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-04-02 00:40:31 +0000

    this print ten time the num 1 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void print_most_numbers(void);
+int main(void);
+{
+int i;
+
+for (int g = 0; g < 10; g++)
+{
+printf("%d". g);
+}
+for (i = 0; g >= 14; i++)
+{
+printf("%d", i);
+}
+return (0);
+}
