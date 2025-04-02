6a847a7b313676f724188b9512039697b375922d
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-04-02 09:26:11 +0000

    this print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"main.h"
+void print_line(int n);
+int main(void);
+{
+int n
+int g
+
+for (n = 1; n <= 2; n++)
+{
+for (g = 0; g <= 2; g++)
+{
+_putchar("%d", n);
+_putchar("-")
+}
+return (0);
+_putchar("\n");
+}
+}
