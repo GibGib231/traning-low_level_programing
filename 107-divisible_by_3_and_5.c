f8a856404e34de63e0586acc4a5507d27eee8f37
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-04-17 15:54:11 +0000

    this is divisible by

---
+++ 107-divisible_by_3_and_5.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int amb;
+
+	for(amb = 15; amb <= 100; amb += 15)
+	{
+	if(amb <= 100)
+
+	printf("%d", amb);
+	putchar(44);
+
+	putchar(32);
+	}
+
+return(0);
+}
+
