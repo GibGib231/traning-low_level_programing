716b3113b9796d5c1ce0fb08269af883522c4f9d
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-04-29 11:45:52 +0000

    this print a string

---
+++ 0x05-pointers_arrays_string/3-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"main.h"
+void _puts(char *str)
+{
+	puts(str);
+	return(0);
+}
+int main()
+{
+//	printf("Amb codeing with is soso cool");
+	_puts("i do not fear computer, but i fear the leack of them");
+	return(0);
+}
