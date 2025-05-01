9acda2045c4f7070b996314abf22888006048e37
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-01 15:54:19 +0000

    this the harder file for the #6 task @amb

---
+++ 0x05-pointers_arrays_string/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void reset_to_98(int *n);
+
+void swap_int(int *a, int *b);
+
+void _puts(char *str);
+
+int _strlen(char *s);
+
+char *_strcpy(char *dest, char *src);
+
+int _atoi(char *s);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+void puts2(char *str);
+
+void print_rev(char *s);
+#endif
