6aaac96963d47c8be0db1ad075a8b29e2695903b
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-01 14:26:56 +0000

    this to concat. tow string. I have underdstand how to use the main.h file and the main file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"main.h"
+#include"string.h"
+char *_strcat(char *dest, char *src)
+{
+	int len1 = strlen(src);
+	int len2 = strlen(dest);
+	int i;
+
+	for(i = 0; i <= len2; i++)
+	{
+		src[len1] = dest[i];
+	}
+	return(dest);
+}
