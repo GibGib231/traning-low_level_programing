43ef16f3df821d267dd7b45d7c4e2d7088283dda
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-02 09:55:41 +0000

    I updated the code as inst.

--- 0x05-pointers_arrays_string/2-strlen.c
+++ 0x05-pointers_arrays_string/2-strlen.c
@@ -1,32 +1,13 @@
 #include<stdio.h>
-#include<string.h>
 #include"main.h"
-int strlen(char *s)
-//this the first way useing a builte in function
-int main()
+int _strlen(char *s)
 {
-	char me[] = "this is a code";
 	int leng;
 
-	leng = strlen(me);
-
-	printf("%d", leng);
-
-	strlen();
-
-	return(0);
-}
-int strlen(char *s)
-{
-	char s = "code with max-well is cool\0";
-
-	int leng;
-
-	for(leng = 0; s[leng] != '\0'leng++);
+	for(leng = 0; s[leng] != '\0'; leng++)
 	{
 	
-	printf("the leng %d\n", leng);
 	}
-	return(leng);
 	
+	return(leng);
 }
