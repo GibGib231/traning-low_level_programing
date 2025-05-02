61c46a5bf8886484305dc958974fd4339a332f18
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-02 09:35:59 +0000

    I change the code and remove the main function, just staring to understand things

--- 0x05-pointers_arrays_string/1-swap.c
+++ 0x05-pointers_arrays_string/1-swap.c
@@ -2,28 +2,10 @@
 void swap_int(int *a, int *b)
 {
 
+	int alg;
 
-
-	return(0);
-}
-int main(){
-
-	int alg = 30;
-	int amb = 40;
-
-
-	printf("a first num %d\nb first num %d\n", alg, amb);
-
-	int a = 33;
-	int b = 44;
-
-	swap_int(&alg, &amb);
-
-	printf("a sec num %d\nb sec num %d\n", a, b); 
-
-
-//	swap_int();
-
-		return(0);
+	alg = *a;
+	*a = *b;
+	*b = alg;
 }
 
