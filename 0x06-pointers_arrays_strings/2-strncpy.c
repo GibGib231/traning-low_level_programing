edd2df74d771161ab8c08f4ccc14ce7d8066a3b1
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-02 18:07:55 +0000

    to copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include"main.h"
+char *_strncpy(char *dest, char *src, int n)
+{
+        int amb = 0;
+	int i = 0;
+	while(src [i] < '\0')
+		src++;
+
+  while (amb < n && src[amb] != '\0') {
+                 dest[amb] = src[amb];
+                 amb++;
+         }
+         dest[amb] = '\0';
+
+
+         return(0);
+}
