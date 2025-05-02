fa279b40171de30651b55a252f3243e691e11261
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-02 18:08:45 +0000

    to compere to string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include"main.h"
+int _strcmp(char *s1, char *s2)
+{
+	int alg = 0;
+
+	while(s1[alg] != '\0')
+	{
+		if(s1[alg] == s2[alg])
+		{
+			alg++;
+		}
+		if(s1[alg] > s2[alg])
+		{
+			return 1;
+	}
+		if(s1[alg] < s2[alg])
+		{
+			return - 1;
+		}
+	}
+	return(0);
+}
