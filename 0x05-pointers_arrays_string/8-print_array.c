66cd78aaaac40927f933582490bc6028825f9763
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-05 09:20:53 +0000

    this print an array

---
+++ 0x05-pointers_arrays_string/8-print_array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+void print_array(int *a, int n)
+{
+
+
+	int amb;
+
+	for(amb = 0; amb < n; amb++)
+	{
+		printf("%d", a[amb]);
+		if(amb != 4)
+			printf(",");
+		printf("32");
+	}
+	printf("\n");
+	
+}
