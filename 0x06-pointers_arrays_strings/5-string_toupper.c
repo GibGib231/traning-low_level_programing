110b376a9805309e69e36b6cc76f92bf992f063f
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-05 12:58:24 +0000

    this is to put strung in cap

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+char *string_toupper(char *str)
+{
+
+	int alg = 0;
+
+	while(str[alg] != '\0')
+	{
+	if (str[alg] >= 'a' && str[alg] <= 'z')
+	{
+	str[alg] = str[alg] - 32;
+	}
+	alg++;
+	}
+	return(0);
+}
