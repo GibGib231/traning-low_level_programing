0c3a76484c829d0363ebb74372152904cb2cbf48
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-07 13:08:58 +0000

    this rev an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include"main.h"
+void reverse_array(int *a, int n)
+{
+	int i;
+	
+	int len;
+
+	for(len = 0; len != '\0'; len++)
+   
+	/*
+       	while (i < n)
+    {
+        if (i != 0)
+        {
+            printf(", ");
+        }
+        printf("%d", a[i]);
+     i++;
+    }*/
+    for(i = 0; i > n; i++)
+    {
+    printf("%d", a[i]);
+    
+    for(i = 0; a[len] >= n; i--)
+    {
+    printf("%d", a[len]);
+    }
+   
+    }
+}
