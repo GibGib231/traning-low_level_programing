c8146986d870c79ae71cf04a1cba761a8022a220
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-07 16:14:09 +0000

    inputded betty styling in the code area

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,20 +3,20 @@ int _strcmp(char *s1, char *s2)
 {
 	int alg = 0;
 
-	while(s1[alg] != '\0')
+	while (s1[alg] != '\0')
 	{
-		if(s1[alg] == s2[alg])
-		{
-			alg++;
-		}
-		if(s1[alg] > s2[alg])
-		{
-			return 1;
+	if (s1[alg] == s2[alg])
+	{
+	alg++;
 	}
-		if(s1[alg] < s2[alg])
-		{
-			return - 1;
-		}
+	if (s1[alg] > s2[alg])
+	{
+	return (1);
 	}
-	return(0);
+	if (s1[alg] < s2[alg])
+	{
+	return (-1);
+	}
+	}
+	return (0);
 }
