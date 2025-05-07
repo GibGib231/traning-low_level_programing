ba75a02510624d35f412f93742937ae9f9885b66
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-07 16:09:17 +0000

    i just included the betty styleing

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,17 +1,16 @@
 #include"main.h"
 char *_strncpy(char *dest, char *src, int n)
 {
-        int amb = 0;
+	int amb = 0;
 	int i = 0;
-	while(src [i] < '\0')
-		src++;
 
-  while (amb < n && src[amb] != '\0') {
-                 dest[amb] = src[amb];
-                 amb++;
-         }
-         dest[amb] = '\0';
+while (src[i] < '\0')
+src++;
 
-
-         return(0);
+while (amb < n && src[amb] != '\0')
+{	 dest[amb] = src[amb];
+	 mb++;
+}
+dest[amb] = '\0';
+return (0);
 }
