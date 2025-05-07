336fd21c4f9661590d79b439b3d7f8344777713e
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-07 13:07:13 +0000

    this replace string to int

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+char *leet(char *str)
+{
+	char cah[10] = "aAeEoOtTlL";
+	char amb[10] = "4433770011";
+	int t;
+	int i;
+
+	for(t = 0; str[t] != '\0'; t++)
+	{
+		for(i = 0; i < 10; i++)
+		{
+		 	if(str[t] == cah[i])
+			{
+			 str[t] = amb[i];
+			}
+		}
+	}
+	return (str);
+}
