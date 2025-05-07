ce0624c9d52fbbe5171c722f50ed0546eedb8379
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-07 16:20:07 +0000

    inputing the betty styleing in the code

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,29 +3,13 @@
 void reverse_array(int *a, int n)
 {
 	int i;
-	
 	int len;
 
-	for(len = 0; len != '\0'; len++)
-   
-	/*
-       	while (i < n)
-    {
-        if (i != 0)
-        {
-            printf(", ");
-        }
-        printf("%d", a[i]);
-     i++;
-    }*/
-    for(i = 0; i > n; i++)
-    {
-    printf("%d", a[i]);
-    
-    for(i = 0; a[len] >= n; i--)
-    {
-    printf("%d", a[len]);
-    }
-   
-    }
+	for (len = 0; len != '\0'; len++)
+	{
+	for (i = 0; i > n; i++)
+	printf("%d", a[i]);
+	for (i = 0; a[len] >= n; i--)
+	printf("%d", a[len]);
+	}
 }
