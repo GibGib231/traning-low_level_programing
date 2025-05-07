2942c40f45e31cb00c422a63a5e398e6f3ea13dc
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-05-07 16:30:23 +0000

    change the commit position

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
+/*
+* *_memset is to put b to s, b is a cons
+ */
 char *_memset(char *s, char b, unsigned int n)
-	/*
-	 * *_memset is to put b to s, b is a cons
-	 */
 {
 unsigned int amb;
 for (amb = '\0'; amb <= n; amb++)
