3d724a89db98b5b7b8a9f88a582edb665ae9a568
Amb Gibson Smith <www.1gibson1smith@gmail.com>
2025-06-04 17:19:49 +0000

    this is to add two num

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc, char *argv[])
+{
+	  if (argc != 3){
+                printf("Error\n");
+        return 1;
+        }
+
+        int amb = atoi(argv[1]);
+        int alg = atoi(argv[2]);
+        int sum = amb + alg;
+
+        printf("the answer is: %d\n", sum);
+        return 0;
+}       
